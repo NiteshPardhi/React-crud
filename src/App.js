7bb1f90e62a834e4a994cb2218135a2ba6099969
nitesh.pardhi <nitesh.pardhi@yamatech.in>
2024-12-17 16:41:10 +0530

    create table and show dummy data in table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import { EmployeeData } from './employeeData';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(EmployeeData)
+  }, []);
+
   return (
     <div className="App">
+
+      <div className='header'>
+        <h1>Employees Data</h1>
+        <button className='btn add_btn' type='submit'>Add Employee</button>
+      </div>
+
+      <div>
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <td>Sr.No</td>
+              <td>First Name</td>
+              <td>Last Name</td>
+              <td>Age</td>
+              <td>Contact No</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.age}</td>
+                    <td>{item.contact}</td>
+                    <td>
+                      <button className='btn btn-primary'>Edit</button>
+                      <button className='btn btn-danger mx-2'>Delete</button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/employeeData.js
@@ -0,0 +1,26 @@
+export const EmployeeData = [
+    {
+        firstName: 'Nitesh',
+        lastName: 'Pardhi',
+        age: 22,
+        contact:76786786
+    },
+    {
+        firstName: 'Shyam',
+        lastName: 'Rajput',
+        age: 82,
+        contact: 78545454
+    },
+    {
+        firstName: 'Raha',
+        lastName: 'Khan',
+        age: 32,
+        contact: 898154154
+    },
+    {
+        firstName: 'Rahul',
+        lastName: 'Dhavan',
+        age: 52,
+        contact: 445454544
+    }
+]
